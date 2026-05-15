4ef3e0ec2df02fc3efa98cb05f2cb1b1472bde56
Jose Cabello Ranea <88961639+josecr02@users.noreply.github.com>
2026-05-15 07:43:26 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        // dp approach, o(1) space and O(n) time.
+        // not necessary
+        int n = temperatures.size();
+        vector<int> res(n, 0);
+
+        for (int i = n - 2; i >= 0; i--) {
+            int j = i + 1;
+            while (j < n && temperatures[j] <= temperatures[i]) {
+                if (res[j] == 0) {
+                    j = n;
+                    break;
+                }
+                j += res[j];
+            }
+
+            if (j < n) {
+                res[i] = j - i;
+            }
+        }
+        return res;
+    }
+};
